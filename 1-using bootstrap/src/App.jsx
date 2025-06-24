ea554f6e050466b7ce02fb3ad6b136a5af440bda
Jyotismita-Das-20 <jyotismitadas68@gmail.com>
2025-06-24 23:00:16 +0530

    create basic UI of Todo list using React and bootstrap

---
+++ 1-using bootstrap/src/App.jsx
@@ -0,0 +1,15 @@
+import AppName from "./components/AppName"
+import AddTodo from "./components/AddTodo"
+function App() {
+
+  return (
+    <>
+      <center>
+        <AppName />
+        <AddTodo />
+      </center>
+    </>
+  )
+}
+
+export default App
